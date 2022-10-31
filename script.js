828e690feaf7a6a9f523e099b084ed3926ae6c5d
abdullah43577 <100965547+abdullah43577@users.noreply.github.com>
2022-10-31 11:29:15 +0100

    just modified some files

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
-const arrowLeft = document.querySelector(".back");
-const arrowRight = document.querySelector(".front");
+// const arrowLeft = document.querySelector(".back");
+// const arrowRight = document.querySelector(".front");
 const images = document.querySelector("#image");
 const imgCont = document.querySelector(".images-container");
 // let index = 0;
@@ -30,8 +30,11 @@ const imagesArr = [
   "image05.jpg",
 ];
 
+// console.log(imagesArr[1]);
+
 setInterval(() => {
-  let random = Math.floor(Math.random() * 5);
+  let random = Math.floor(Math.random() * imagesArr.length);
+  // console.log(random);
   images.src = imagesArr[random];
 }, 3000);
 
