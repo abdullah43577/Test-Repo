34dbb5e852d6e30c6af6a983e6f6dedc7af8600a
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2022-11-01 15:00:40 +0100

    made some few changes to the code

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 "use strict";
-// const arrowLeft = document.querySelector(".back");
-// const arrowRight = document.querySelector(".front");
 const images = document.querySelector("#image");
 const imgCont = document.querySelector(".images-container");
-// let index = 0;
-
-// arrowRight.addEventListener("click", () => {
-//   index++;
-
-//   if (index > array.length - 1) {
-//     index = 0;
-//   }
-//   images.src = array[index];
-// });
-
-// arrowLeft.addEventListener("click", () => {
-//   index--;
-//   if (index < 0) {
-//     index = 4;
-//   }
-//   images.src = array[index];
-// });
 
 const imagesArr = [
   "image01.jpeg",
@@ -32,19 +12,8 @@ const imagesArr = [
 
 // console.log(imagesArr[1]);
 
-setInterval(() => {
+setInterval(function () {
   let random = Math.floor(Math.random() * imagesArr.length);
-  // console.log(random);
+  console.log(random);
   images.src = imagesArr[random];
 }, 3000);
-
-/* PLEASE IGNORE THIS SECTION */
-
-// function isSubstring(searchString, subString) {
-//   const char = subString.toLowerCase();
-//   const string = searchString.toLowerCase();
-//   const result = char.includes(string);
-//   console.log(result);
-// }
-
-// isSubstring("O", "dog");
